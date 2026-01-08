e9c1e97b10cd132e7d36700caf99a5f43a27ae6e
Peter Hallonmark <peter.hallonmark@gmail.com>
2026-01-08 23:02:06 +0100

    Made it possible to build clang-tidy-standalone.

--- ClangTidyForceLinker.h
+++ ClangTidyForceLinker.h
@@ -9,16 +9,11 @@
 #ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANGTIDYFORCELINKER_H
 #define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANGTIDYFORCELINKER_H
 
-#include "clang/Config/config.h"
+#include "clang-tidy-config.h"
 #include "llvm/Support/Compiler.h"
 
-namespace clang {
-namespace tidy {
-// This anchor is used to force the linker to link the MiscModule.
-extern volatile int MiscModuleAnchorSource;
-static int LLVM_ATTRIBUTE_UNUSED MiscModuleAnchorDestination =
-    MiscModuleAnchorSource;
-} // namespace tidy
-} // namespace clang
+namespace clang::tidy {
+
+} // namespace clang::tidy
 
 #endif
